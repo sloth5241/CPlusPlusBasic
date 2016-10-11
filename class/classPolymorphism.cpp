09be8435a543070db6948a73b6452ea7e577931b
sloth5241 <zhaocong5241@126.com>
2016-10-11 14:10:18 +0800

    多态和虚函数

--- class/classPolymorphism.cpp
+++ class/classPolymorphism.cpp
@@ -4,7 +4,20 @@ using namespace std;
 
 int main() {
 
-    Line line1;
-    
+    double tempArea = 0;
+    Shape *shape;
+    Rectangle rec(10,7);
+    Triangle  tri(10,5);
+
+    // 存储矩形的地址
+    shape = &rec;
+    // 调用矩形的求面积函数 area
+    tempArea = shape->area();
+    cout << " area is " << tempArea << endl;
+    // 存储三角形的地址
+    shape = &tri;
+    // 调用三角形的求面积函数 area
+    tempArea = shape->area(); 
+    cout << " area is " << tempArea << endl;
     return 0;
 }
--- class/classPolymorphism.h
+++ class/classPolymorphism.h
@@ -4,11 +4,38 @@
 using namespace std;
 #endif
 
-class Line {
+class Shape {
+    protected:
+        double width,height;
     public:
-        Line() {
-            cout << "obj is created" << endl; 
-        };
+        Shape(double width=0, double height=0) {
+            this->width = width;
+            this->height = height;
+        }
 
+        virtual double area() {
+            cout << "parent area is: " << endl;
+            return (width * height);
+        }
+};
+
+class Rectangle: public Shape {
+    public:
+        Rectangle( double width=0, double height=0):Shape(width, height) { }
+        double area ()
+        { 
+            cout << "Rectangle class area :" <<endl;
+            return (width * height); 
+        }
+};
+
+class Triangle: public Shape{
+    public:
+        Triangle( double width=0, double height=0):Shape(width, height) { }
+        double area ()
+        { 
+            cout << "Triangle class area :" <<endl;
+            return (width * height / 2); 
+        }
 };
 
