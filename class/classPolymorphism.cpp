7ef824fe8001812e36fa8abd4cb1983b83c0aeaa
sloth5241 <zhaocong5241@126.com>
2016-10-11 13:41:29 +0800

    add new classPolymorphism files

---
+++ class/classPolymorphism.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "classPolymorphism.h"
+using namespace std;
+
+int main() {
+
+    Line line1;
+    
+    return 0;
+}
---
+++ class/classPolymorphism.h
@@ -0,0 +1,14 @@
+#ifndef classPolymorphism_h
+#define classPolymorphism_h
+#include <iostream>
+using namespace std;
+#endif
+
+class Line {
+    public:
+        Line() {
+            cout << "obj is created" << endl; 
+        };
+
+};
+
