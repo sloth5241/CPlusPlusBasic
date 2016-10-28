78397c448a1fd209538fad19dbefa159c8aa0c2c
sloth5241 <zhaocong5241@126.com>
2016-10-28 15:32:45 +0800

    字符数组

--- character/char.cpp
+++ character/char.cpp
@@ -1,23 +1,55 @@
 #include <iostream>
+#include<cstring>
 using namespace std;
-/*
- *字符串的两种定义方法
- */
-int main() {
-    //可以修改字符串元素
-    char c1[5] = "zhao";
-    char c2[5] = "cong";
-    c1[3] = '1';
-    cout << c1 << " " << c2 << endl;
+const int n = 5;
 
-    const char* c11 = "xu";
-    const char* c22 = "li";
-    cout << c11 << " " << c22 << endl;
+void ergodicCharArray(char* c1, char* c2){
+    for(int i=0; i<n; i++){
+        cout << "char is:" << c1[i] << endl;
+    }  
+    for(int i=0; i<n; i++){
+        cout << "char is:" << c2[i] << endl;
+    }  
+}
 
+char* modifyCharArray(char* c1){
+    c1[0] = 'm';
+    c1[1] = 'o';
+    c1[2] = 'd';
+    c1[3] = 'i';
+    c1[4] = 'f';
+    c1[5] = 'y'; 
+    return c1;
+}
+
+char* strcatCharArray(char* c1, char* c2){
     strcat(c1,c2);
-    cout << "连接后c1长度:" << c1 << endl;
+    return c1;
+}
 
-    int len = strlen(c1);
-    cout << "c1 length:" << len << endl;
+int getCharArrayLength(char* c1){
+    return strlen(c1);
+}
+
+int main() {
+    //字符数组
+    char char1[n] = "zhao";
+    char char2[n] = "cong";
+    //遍历字符数组
+    ergodicCharArray(char1, char2);
+
+    //连接字符数组
+    char* retStrcat = strcatCharArray(char1, char2); 
+    cout << "连接后的字符串为:" << retStrcat << endl;
+
+    //修改字符数组元素
+    char* retModify = modifyCharArray(char1);
+    cout << "修改后的字符数组为:" << retModify << endl;
+
+    //字符数组长度
+    cout << char1 << endl;
+    int char1Length = getCharArrayLength(char1);
+    cout << "char1的长度为:" << char1Length << endl;
+
     return 0;
 }
