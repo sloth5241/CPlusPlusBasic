7ec09ea71c796eaaa89cbb04bd791ecc0f4fcefc
sloth5241 <zhaocong5241@126.com>
2016-10-31 00:22:31 +0800

    const 两种方式

--- const/const.cpp
+++ const/const.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    const double *p = 0;
+void constValue() {
+    const double* p = 0;
     const double len = 12.232;
 
     cout << "p = " << p << endl;
@@ -14,10 +13,23 @@ int main() {
     cout << "p = " << p << endl;
     cout << "*p = " << *p << endl;
 
-
     double dval = 3.14;
     p = &dval;
     cout << "p=" << p << endl;
     cout << "*p=" << *p << endl;
+}
+
+void constPoint() {
+    double len = 123.213;
+    double wid = 90123.89;
+    double* const p = &len;
+    cout << "*p = " << *p << endl;
+
+    //p = &wid; //error
+}
+
+int main() {
+    //constValue();
+    constPoint();
     return 0;
 }
