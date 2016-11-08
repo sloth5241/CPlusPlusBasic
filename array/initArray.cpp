17c39f055303e6a7898c47be37a0d4acbc0b6cc7
sloth5241 <zhaocong5241@126.com>
2016-11-08 21:15:55 +0800

    初始化数组完善

--- array/initArray.cpp
+++ array/initArray.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int arr1[3] = {1,2,3};
-int arr2[3] = {11};
+const int n = 3;
+int arr1[n] = {1,2,3};
+int arr2[n] = {11};
 
 int main () {
-    for (int i=0; i<3; i++) {
-        cout << "arr1:" << arr1[i] << endl;
-        cout << "arr2:" << arr2[i] << endl;
-        cout << "--------------------" << endl;
+    try{
+        for (int i=0; i<n; i++) {
+            cout << "arr1:" << arr1[i] << endl;
+            cout << "arr2:" << arr2[i] << endl;
+            cout << "--------------------" << endl;
+        }
+    }
+    catch(const char* msg){
+        cout << msg << endl;
     }
 
     return 0;
