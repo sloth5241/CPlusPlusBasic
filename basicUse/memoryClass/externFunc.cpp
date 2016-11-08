dfc711188f011e977784fa16aaa73a62d1d2b47d
sloth5241 <zhaocong5241@126.com>
2016-11-08 21:12:21 +0800

    添加异常处理和移动static目录

--- basicUse/memoryClass/externFunc.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-extern int total;
-
-void getTotal() {
-
-    cout << "total is:" << total << endl;
-}
--- basicUse/randOpt.cpp
+++ basicUse/randOpt.cpp
@@ -4,18 +4,21 @@
 using namespace std;
 
 int main() {
-    
-    int i,j;
-     
-    // 设置种子
-    srand( (unsigned)time( NULL ) );
+    try{ 
+        int i,j;
 
-    /* 生成 10 个随机数 */
-    for( i = 0; i < 10; i++ )
-    {
-        // 生成实际的随机数
-        j= rand()%100;    //%后面数字代表取值范围
-        cout <<"随机数： " << j << endl;
+        // 设置种子
+        srand( (unsigned)time( NULL ) );
+
+        /* 生成 10 个随机数 */
+        for( i = 0; i < 10; i++ ){
+            // 生成实际的随机数
+            j= rand()%100;    //%后面数字代表取值范围
+            cout <<"随机数： " << j << endl;
+        }
+    }
+    catch(const char* msg) {
+        cout << "exception msg:" << msg << endl;
     }
     return 0;
 }
