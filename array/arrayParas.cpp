901aa6137160fd15ebed3b8ba73451c16f6f81a7
sloth5241 <zhaocong5241@126.com>
2016-11-08 21:27:18 +0800

    完善数组传递数组参数

--- array/arrayParas.cpp
+++ array/arrayParas.cpp
@@ -4,33 +4,46 @@ using namespace std;
  *传递数组给函数的方式
  */
 
-//定义数组
 int* retArr1(int arr[5], int n) {
+    /*
+     * 使用数组接收数组
+     */
     for(int i=0; i<n; i++) {
         arr[i] += 1;
     }
     return arr;
 }
 
-//定义指针
 int * retArr2(int *p, int n) {
+    /*
+     * 使用指针接收数组
+     */
     for(int i=0; i<n; i++) {
         p[i] += 100;
     }
     return p;
 }
 
+void printArray(int *p, int n) {
+   for(int i=0; i<n; i++) {
+        cout << p[i] << endl;
+   } 
+}
+
 int main() {
-    int array0[5] = {1,2,3,4,5};
-    cout << "使用数据接收" << endl;
-    int *p = retArr1(array0,5); 
-    for(int i=0; i<5; i++) {
-        cout << "retArr1 is:" << p[i] << endl;
+    try {
+        const int n = 5;
+        int array0[n] = {1,2,3,4,5};
+        cout << "使用数据接收" << endl;
+        int *p = retArr1(array0,n); 
+        printArray(p, n);
+
+        cout << "使用指针接" << endl;
+        int *q = retArr2(array0,n);
+        printArray(p, n);
     }
-    cout << "使用指针接" << endl;
-    int *q = retArr2(array0,5);
-    for(int i=0; i<5; i++) {
-        cout << "retArr2 is:" << q[i] << endl;
+    catch(const char* msg){
+        cout << msg << endl;      
     }
     return 0;
 }
