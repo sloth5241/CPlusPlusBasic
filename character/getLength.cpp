b4b479d4918724c0ad13f2a28c47627792288f1e
sloth5241 <zhaocong5241@126.com>
2016-11-08 21:41:44 +0800

    完善获取字符串长度的代码

--- character/getLength.cpp
+++ character/getLength.cpp
@@ -2,13 +2,21 @@
 using namespace std;
 
 int main() {
-    const char* c1 = "zhaocong";
-    cout <<" c1 is a const char" << endl;
-    cout << "size of c1 :" << sizeof(c1) << endl; 
-    cout << "strlen of c1: " << strlen(c1) << endl;
+    try{
+        const char* c1 = "zhaocong";
+        cout <<" c1 is a const char" << endl;
+        cout << "size of c1 :" << sizeof(c1) << endl; 
+        cout << "strlen of c1: " << strlen(c1) << endl;
 
-    char c2[20] = "zhaocong";
-    cout << "sizeof c2:" << sizeof(c2) << endl;
-    cout << "strlen c2:" << strlen(c2) << endl;
+        cout << "------------------------" << endl;
+
+        char c2[20] = "zhaocong";
+        cout <<" c2 is a array char" << endl;
+        cout << "sizeof c2:" << sizeof(c2) << endl;
+        cout << "strlen c2:" << strlen(c2) << endl;
+    }
+    catch(const char* msg) {
+        cout << msg << endl;
+    }
 
 }
