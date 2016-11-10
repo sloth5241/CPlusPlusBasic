41125f0eedce93940b9440d50f82e99d0026eac6
sloth5241 <zhaocong5241@126.com>
2016-11-10 10:41:20 +0800

    类对象的初始化

--- class/classDefinite.cpp
+++ class/classDefinite.cpp
@@ -4,29 +4,20 @@ using namespace std;
 
 int main() {
     Box box1;
-    Box box2;
-    double volum1 = 0;    //volum of box1
-    double volum2 = 0;    //volum of box2
+    Box *box2 = new Box;
+    try{
+        box1.length = 1;
+        box2->length = 10;
 
-    box1.length = 10.9;
-    box1.breadth = 32.2;
-    box1.height = 12.12;
-    box2.length = 20.9;
-    box2.breadth = 0;
-    box2.height = 1.12;
-    
-    volum1 = box1.length * box1.breadth * box1.height;
-    volum2 = box2.length * box2.breadth * box2.height;
-    
-    if (0 != volum1) {
-        cout << "the volum of box1 is: " << volum1 << endl;
-    }else {
-        cout << "please check the properties of box1" << endl; 
+        cout << "box1.length = " << box1.length << endl;
+        cout << "box2->length = " << box2->length << endl; 
+
+        (*box2).length = 200;
+        cout << "(*box2).length = " << (*box2).length << endl; 
+        
     }
-    if (0 != volum2) {
-        cout << "the volum of box1 is: " << volum2 << endl;
-    }else {
-        cout << "please check the properties of box2" << endl; 
+    catch(const char* msg) {
+        cout << msg << endl;
     }
     return 0;
 }
--- class/classDefinite.h
+++ class/classDefinite.h
@@ -1,17 +1,16 @@
 /**
  *  learn how to definite a c++ class
+ *  类指针指向类对象
  **/
 #ifndef classDefinite_h
 #define classDefinite_h
 #include <iostream>
 using namespace std;
-#endif /* classDefinite.h */
 
 class Box {
     public:
         double length;
-        double breadth;
-        double height; 
 };
 
+#endif /* classDefinite.h */
 
